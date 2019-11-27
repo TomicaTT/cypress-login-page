ee9cbe6ae7fdacb41224a2d29bc791a8bda0f6e4
Stefan <stefan1612@its.edu.rs>
2019-11-27 15:30:36 +0100

    Test successful login

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -4,3 +4,22 @@ describe('visit login page', function() {
         cy.contains('Word Nerds')
     })
 })
+
+describe('sign up', function() {
+    it('clicks on sign up', function() {
+        cy.visit('https://word-nerds-client.herokuapp.com')
+        cy.contains('Sign Up').click()
+        cy.url().should('include', '/register')
+    })
+})
+
+describe('login to app', function() {
+    it('enter username and password and submit form', function() {
+        cy.visit('https://word-nerds-client.herokuapp.com')
+        cy.get('input:first').should('have.attr', 'placeholder', 'Username')
+          .type('bob')
+        cy.get('input:last').should('have.attr', 'placeholder', 'Password')
+          .type('bob')
+        cy.get('form').submit()
+    })
+})
